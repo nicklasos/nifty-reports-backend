b692981a4f813108dff3b0a9ea30350291810d46
nicklasos <github7415963>
2022-05-15 12:19:36 +0300

    Add test colelction

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
-const {connect, close} = require('./src/mongo');
+const {connect, close, getConnection} = require('./src/mongo');
 const {calculateAndSaveStats} = require("./src/opensea_stats");
 
 async function run() {
 	try {
 		await connect();
 
-		await calculateAndSaveStats('everai-heroes-duo', 1);
+		// await calculateAndSaveStats('everai-heroes-duo', 1);
+
+		await getConnection().collection('collection').insertOne({
+			collection_slug: 'everai-heroes-duo',
+			id: 1,
+		})
 
 	} finally {
 		await close();

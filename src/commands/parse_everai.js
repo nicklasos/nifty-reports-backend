1ff233ba4c8888d886a4f70d92fc96118ce001cf
todya <todor.tsnkv@gmail.com>
2022-06-10 19:27:05 +0100

    extract collection details into separate file

--- src/commands/parse_everai.js
+++ src/commands/parse_everai.js
@@ -10,10 +10,9 @@ const {parseIcyToolsCollectionStats} = require("../icy_tools");
 // const {parseOptickCommunitySize} = require("../optick");
 const {mergeData} = require("../merge_data");
 
-const collectionSlug = 'everai-heroes-duo';
-const contractAddress = '0x9a38DEC0590aBC8c883d72E52391090e948DdF12';
-const collectionSize = 7777;
-const twitterHandle = 'TheEverai';
+const { collectionDetails } = require("../collection-details");
+
+const { collectionSlug, contractAddress, collectionSize, twitterHandle  } = collectionDetails['everai'];
 
 async function run() {
 	try {

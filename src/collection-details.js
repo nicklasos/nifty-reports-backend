5f567d6ad0d871beac4a7b3ddc5c2965d0353cd9
nicklasos <nicklasos@gmail.com>
2022-07-28 08:22:36 +0300

    Parse all enabled collection at once

--- src/collection-details.js
+++ src/collection-details.js
@@ -4,30 +4,35 @@ const collectionDetails = {
     contractAddress: '0x9a38DEC0590aBC8c883d72E52391090e948DdF12',
     collectionSize: 7777,
     twitterHandle: 'TheEverai',
+    enabled: true,
   },
   aswang: {
     collectionSlug: 'theaswangtribe',
     contractAddress: '0xa462127735352B1F03dA8Ab92a87803d05cc6a7B',
     collectionSize: 3333,
     twitterHandle: 'AswangTribe',
+    enabled: false,
   },
   childrenofukiyo: {
     collectionSlug: 'childrenofukiyoofficial',
     contractAddress: '0x3b3C2daCfDD7b620C8916A5f7Aa6476bdFb1aa07',
     collectionSize: 10000,
     twitterHandle: 'childrenofukiyo',
+    enabled: true,
   },
   possessed: {
     collectionSlug: 'thepossessed',
     contractAddress: '0x3a2096754df385553c4252e5a82dc862e64169bb',
     collectionSize: 10000,
     twitterHandle: 'ThePossessedNFT',
+    enabled: false,
   },
   zenogakki: {
     collectionSlug: 'zenogakki-official-genesis',
     contractAddress: '0x6372e41cd6415261cf38daccefed2f0cc206c282',
     collectionSize: 10000,
     twitterHandle: 'zenogakki',
+    enabled: true,
   },
 };
 
---
+++ src/commands/parse_enabled.js
@@ -0,0 +1,21 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '..', '.env') });
+
+const { connect, close } = require('./../mongo');
+
+const { collectionDetails } = require('../collection-details');
+const { parse } = require('./parse');
+
+async function run() {
+  try {
+    await connect();
+
+    for (const [name, collection] of Object.entries(collectionDetails)) {
+      await parse(collection);
+    }
+  } finally {
+    await close();
+  }
+}
+
+run().catch(console.dir);
